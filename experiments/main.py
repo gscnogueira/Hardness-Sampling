0591606bfac2188576c6ba06054cd402f5341323
Gabriel da Silva Corvino Nogueira <gabrielscnogueira@gmail.com>
2024-06-05 14:49:28 -0300

    ✨ (Experimentos) Adiciona estratégias clássicas

--- experiments/main.py
+++ experiments/main.py
@@ -2,27 +2,34 @@ import os
 from functools import partial
 
 import pandas as pd
-from modAL.uncertainty import uncertainty_sampling
 from pyhard.measures import ClassificationMeasures
-
-from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from modAL.uncertainty import margin_sampling
+from tqdm import tqdm
 
 from config import CSV_DIR, RESULTS_DIR
 from active_learning_experiment import ActiveLearningExperiment
+from strategies.expected_error import expected_error_reduction
+from strategies.information_density import (density_weighted_sampling,
+                                            training_utility_sampling)
 
 
-def run_exeriments(dataset_file, estimator, query_strategy):
+def run_exeriments(dataset_file, estimator, query_strategy,
+                   n_queries=100, initial_labeled_size=5,
+                   random_satate=None):
 
     df = pd.read_csv(os.path.join(CSV_DIR, dataset_file))
 
-    exp = ActiveLearningExperiment(df, initial_labeled_size=5,
-                                   n_queries=100, random_state=0)
+    exp = ActiveLearningExperiment(data=df,
+                                   initial_labeled_size=initial_labeled_size,
+                                   n_queries=n_queries,
+                                   random_state=random_satate)
 
     scores = exp.run_strategy(estimator=estimator,
                               query_strategy=query_strategy)
 
     dataset_name, _ = os.path.splitext(dataset_file)
+
     file_name = (f'{dataset_name}_{estimator.__name__}'
                  f'_{query_strategy.__name__}.csv')
 
@@ -31,8 +38,27 @@ def run_exeriments(dataset_file, estimator, query_strategy):
 
 if __name__ == '__main__':
 
+    sampling_methods = [
+        margin_sampling,
+        density_weighted_sampling,
+        training_utility_sampling,
+        expected_error_reduction,
+    ]
+
     datasets = (f for f in os.listdir(CSV_DIR))
 
-    run_exeriments(next(datasets),
-                   KNeighborsClassifier,
-                   uncertainty_sampling)
+    dataset = next(datasets)
+    estimator = KNeighborsClassifier
+
+    for method in (pbar := tqdm(sampling_methods)):
+
+        pbar.set_description("["
+                             f"{dataset}, "
+                             f"{estimator.__name__}, "
+                             f"{method.__name__}"
+                             "]")
+
+        run_exeriments(dataset,
+                       estimator=estimator,
+                       query_strategy=method,
+                       n_queries=10)
