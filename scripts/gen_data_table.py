e7159dff7e5af6c802d2585dacfdc701a0959f85
Gabriel da Silva Corvino Nogueira <gabrielscnogueira@gmail.com>
2024-08-16 15:48:15 -0300

    ♻️  Refatora script de geração de tabela

--- scripts/gen_data_table.py
+++ scripts/gen_data_table.py
@@ -6,6 +6,11 @@ from scipy.io import arff
 DATASET_DIR = '../datasets/arff/'
 DATASET_LIST_FILE = '../datasets/datasets.txt'
 
+
+def truncate_text(text, max_lenght=22):
+    return text if len(text) < max_lenght else text[:max_lenght - 3] + '...'
+
+
 if __name__ == '__main__':
 
     # arff_files = os.listdir(DATASET_DIR)
@@ -13,13 +18,13 @@ if __name__ == '__main__':
     dataset_names = sorted([f.strip() for f in open(DATASET_LIST_FILE)])
 
     records = []
-    for dataset in dataset_names:
+    for i, dataset in enumerate(dataset_names, 1):
         data , meta_data = arff.loadarff(os.path.join(DATASET_DIR,
                                                       dataset + ".arff"))
 
         row = {}
         # Nome do dataset (sem -, e extensao)
-        row['Name'] = dataset
+        row['Name'] = f'{str(i)}-{dataset}'
 
         # Quantidade de instancias
         row['#in'] = len(data)
@@ -38,8 +43,14 @@ if __name__ == '__main__':
 
     split_point = 45
     df = pd.DataFrame().from_records(records)
-    df['Name'] = df['Name'].apply(lambda x: x[:5])
 
-    print(df.iloc[:split_point].to_latex())
-    print('--------------------')
-    print(df.iloc[split_point:].to_latex())
+    # import pdb; pdb.set_trace()
+    formatters = {"Name": truncate_text}
+    df.iloc[:split_point].to_latex(index=False, formatters=formatters,
+                                   buf=open('datasets_table.tex', 'w'))
+    df.iloc[split_point:].to_latex(index=False, formatters=formatters,
+                                   buf=open('datasets_table.tex', 'a'))
+    df.describe().to_latex(buf=open('datasets_stats_table.tex',
+                                    'w'),
+                           float_format="%.2f",
+                           escape=True)
